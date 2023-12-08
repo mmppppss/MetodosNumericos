a25fe9fbf55c96b78aa93a195f4e096000f8d9ea
mmppppss <ppozosoliz@gmail.com>
2023-12-07 21:21:19 -0400

    cambios radicales

--- graph.py
+++ graph.py
@@ -1,4 +1,9 @@
 import planoCartesiano as PC
 import metodos, math
-#print(metodos.fxpoints( "ln(x)", -10, 5))
+#print(str(metodos.fxpoints( "ln(x)", -10, 5)).replace(", ","\n"))
+print(metodos.solve("ln(x)", 5));
+print(metodos.solve("tan(x)", 5));
+print(metodos.solve("e^(x)", 5));
+print(metodos.solve("sec(x)", 5));
 a =PC.PlanoCartesiano(800, 500, 10, "Plano Cartesiano")
+metodos.biseccion(1,2,"x**5+4*x**2-10",0.01)
--- metodos.py
+++ metodos.py
@@ -1,5 +1,30 @@
 import math
 
+def biseccion(xl:float, xu:float, fx:str, es:float):
+    """
+    Encuentra la raiz de una ecuacion con el metodo de la biseccion
+    Args:
+        xl (float): Limite inferior
+        xu (float): Limite superior
+        fx (str): Funcion
+        es (int): Error permitido
+    """
+    list=[(str("XL"),str("XU"),str("FX(XL)"),str("FX(XU)"),str("XR"),str("EA"))]
+    xr=(xl+xu)/2
+    ea=100
+    while ea>es:
+        if solve(fx, xl)*solve(fx, xr)<0:
+            xu=xr
+        else:
+            xl=xr
+        xr=(xl+xu)/2
+        ea=abs((xr-xl)/xr)*100
+        print(round(xl, 3), "\t", round(xu, 3), "\t", round(solve(fx, xl), 3), "\t", round(solve(fx, xu), 3), "\t", round(xr,3),"\t", round(ea, 3))
+        li=(str(round(xl, 3)), str(round(xu, 3)), str(round(solve(fx, xl), 3)), str(round(solve(fx, xu), 3)), str(round(xr,3)), str(round(ea, 3)))
+        list.append(li)
+    return list
+
+
 def newtonRaphson(x0, f, fder, epsilon):
     x = x0
     while True:
@@ -7,6 +32,7 @@ def newtonRaphson(x0, f, fder, epsilon):
         if abs(f(x)) < epsilon:
             break
     return x
+
 def fxpoints(fx:str, inicioX:int, finX:int):
     """
     Calcula los valores de una funcion y retorna una lista con los valores en y
@@ -21,12 +47,25 @@ def fxpoints(fx:str, inicioX:int, finX:int):
         print("[#] ERROR: ",e)
     return points
 
+def solve(fx, x):
+    return eval(evaluate(fx))
+
 def evaluate(fx:str):
     """
     Evalua un string "funcion" en lenguaje natural y
     la convierte a lenguaje python 
     """
-    fxfinal = fx.replace("sin", "math.sin").replace("cos", "math.cos").replace("tan", "math.tan").replace("sqrt", "math.sqrt").replace("log","math.log10").replace("ln","math.log").replace("e^","math.exp")
+    fxfinal = fx.replace("sin", "math.sin")\
+            .replace("cos", "math.cos")\
+            .replace("tan", "math.tan")\
+            .replace("sqrt", "math.sqrt")\
+            .replace("log","math.log10")\
+            .replace("ln","math.log")\
+            .replace("e^","math.exp")\
+            .replace("^","**")\
+            .replace("sec","1/math.cos")\
+            .replace("cosec","1/math.sin")\
+            .replace("cot","1/math.tan")
     return fxfinal
 
 def mathError(fx):
